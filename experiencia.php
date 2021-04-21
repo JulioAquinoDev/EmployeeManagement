3296d1340ba79e63e42d8e5eb9029145be95e0a1
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2021-04-21 02:21:02 -0300

    update - tela de login

--- experiencia.php
+++ experiencia.php
@@ -1,43 +1,52 @@
 <?php
-    require_once "conexao.php";
-    class Experiencia{
-        private $idexperiencia;
-        private $tipo;
-        private $quantidade;
-        private $tempo;
+require_once "conexao.php";
+class Experiencia
+{
+    private $idexperiencia;
+    private $tipo;
+    private $quantidade;
+    private $tempo;
 
-        public function setIdExperiencia($idexperiencia){
-            $this->idexperiencia = $idexperiencia;
-        }
-        public function getIdExperiencia(){
-            return $this->idexperiencia;
-        }
-        public function setTipo($tipo){
-            $this->tipo = $tipo;
-        }
-        public function getTipo(){
-            return $this->tipo;
-        }
-        public function setQuantidade($quantidade){
-            $this->quantidade = $quantidade;
-        }
-        public function getQuantidade(){
-            return $this->quantidade;
-        }
-        public function setTempo($tempo){
-            $this->tempo = $tempo;
-        }
-        public function getTempo(){
-            return $this->tempo;
-        }
-
-        public inserir(){
-
-        }
-        public alterar(){
-
-        }
-        public remover(){
-            
-        }
+    public function setIdExperiencia($idexperiencia)
+    {
+        $this->idexperiencia = $idexperiencia;
     }
+    public function getIdExperiencia()
+    {
+        return $this->idexperiencia;
+    }
+    public function setTipo($tipo)
+    {
+        $this->tipo = $tipo;
+    }
+    public function getTipo()
+    {
+        return $this->tipo;
+    }
+    public function setQuantidade($quantidade)
+    {
+        $this->quantidade = $quantidade;
+    }
+    public function getQuantidade()
+    {
+        return $this->quantidade;
+    }
+    public function setTempo($tempo)
+    {
+        $this->tempo = $tempo;
+    }
+    public function getTempo()
+    {
+        return $this->tempo;
+    }
+
+    public function inserir()
+    {
+    }
+    public function alterar()
+    {
+    }
+    public function remover()
+    {
+    }
+}
